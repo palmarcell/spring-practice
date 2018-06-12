1097ebbb40549feabd8c2a762d6b74761f05a22f
palmarcell <palmarcell96@gmail.com>
2018-06-12 13:14:11 +0200

    including 2 items to db

--- src/main/java/com/palmarcell/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/palmarcell/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,12 +2,25 @@ package com.palmarcell.spring5webapp.bootstrap;
 
 import com.palmarcell.spring5webapp.model.Author;
 import com.palmarcell.spring5webapp.model.Book;
+import com.palmarcell.spring5webapp.repositories.AuthorRepository;
+import com.palmarcell.spring5webapp.repositories.BookRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * Created by gemboly on 2018. 06. 12., 2018
  */
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
-public class DevBootstrap {
+    private AuthorRepository authorRepository;
+    private BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
 
     private void initData() {
 
@@ -16,9 +29,21 @@ public class DevBootstrap {
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
         Author rod = new Author("Red","Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
         rod.getBooks().add(noEJB);
 
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
     }
 }
