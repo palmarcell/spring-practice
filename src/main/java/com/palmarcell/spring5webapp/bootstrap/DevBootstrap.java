d538901ee4772270c091c7a4661b037270b36884
palmarcell <palmarcell96@gmail.com>
2018-06-12 19:43:06 +0200

    refactored and extended Publisher

--- src/main/java/com/palmarcell/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/com/palmarcell/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package com.palmarcell.spring5webapp.bootstrap;
 
 import com.palmarcell.spring5webapp.model.Author;
 import com.palmarcell.spring5webapp.model.Book;
+import com.palmarcell.spring5webapp.model.Publisher;
 import com.palmarcell.spring5webapp.repositories.AuthorRepository;
 import com.palmarcell.spring5webapp.repositories.BookRepository;
+import com.palmarcell.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -16,24 +18,34 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     private void initData() {
 
+        Publisher publisher = new Publisher();
+        publisher.setName("Harper Collins");
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric","Evans");
-        Book ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        Book ddd = new Book("Domain Driven Design", "1234", publisher);
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
         authorRepository.save(eric);
         bookRepository.save(ddd);
 
+        Publisher publisher2 = new Publisher();
+        publisher2.setName("Worx");
+        publisherRepository.save(publisher2);
+
         Author rod = new Author("Red","Johnson");
-        Book noEJB = new Book("J2EE Development without EJB", "23444", "Worx");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", publisher2);
         rod.getBooks().add(noEJB);
 
         authorRepository.save(rod);
---
+++ src/main/java/com/palmarcell/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,12 @@
+package com.palmarcell.spring5webapp.repositories;
+
+import com.palmarcell.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by gemboly on 2018. 06. 12., 2018
+ */
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+
+}
